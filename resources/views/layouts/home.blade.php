0d32bfa1ea0ddaa6ad46fb4e37327d478b1046e6
handblack <soporte@miasoftware.net>
2021-01-03 20:17:25 -0500

    nuevo theeme

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+        <meta name="generator" content="Hugo 0.79.0">
+        <title>Carousel Template · Bootstrap v5.0</title>
+
+        <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    
+    @yield('header')
+    @yield('style')
+    </head>
+    <body>
+        @include('layouts.navbar')
+        <main>
+            @yield('container')
+       
+            @include('layouts.footer')
+        </main>
+        <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>      
+    </body>
+</html>
