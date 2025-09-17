d7148c61b02a63f8f738d6ed9e9e99d9dc680c5b
Luis Angel <luissb04.official@gmail.com>
2025-09-16 22:34:18 -0500

    Descripción de los cambios

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.page')
 
 @section('header')
@@ -26,71 +27,70 @@
         <div class="col-lg-12 ">
             <iframe class="mb-4 mb-lg-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sCalle+Pampilla+121!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
 
-
           </div>
     </section>
 
     <section id="contact">
         <div class="container">
 
-
             <div class="row">
-                <div class="col-8">
-                    <form action="" method="POST" role="form">
-                        <div class="form-group row mt-3">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Nombres</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputPassword" placeholder="Nombres y apellidos">
-                            </div>
-                        </div>
-                        <div class="form-group row mt-3">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">E-Mail</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputPassword" placeholder="Correo electronico">
-                            </div>
-                        </div>
-                        <div class="form-group row mt-3">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Telefono/Movil</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputPassword" placeholder="Telefono / Movil">
-                            </div>
-                        </div>
-                        <div class="form-group row mt-3">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Mensaje</label>
-                            <div class="col-sm-10">
-                                <textarea rows="10" cols="50" class="form-control"></textarea>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-4">
-                    <strong>CORREO</strong>
-                    <br>ganasco@esg.com.pe
-                    <br>ventas2@esg.com.pe
-                    <hr>
-                    <strong>Telefonos</strong>
-                    <br>(+51) 1 713 1630
-                    <br>953 928 070
-
-                    <hr><strong>HORARIO</strong>
-                    <table width="100%">
-                        <tr>
-                            <td>
-                                Lunes - Viernes
-                                <br>8:15 a.m. a 6:00 p.m.
-                            </td>
-                            <td>
-                                Sabado
-                                <br>8:15 a.m. a 12:30 p.m.
-
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+  <!-- Formulario de Contacto Compacto -->
+<div class="col-8">
+    <form action="" method="POST" role="form">
+        @csrf
+        <div class="form-group row mb-2">
+            <label for="name" class="col-sm-2 col-form-label">Nombres</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control form-control-sm" id="name" name="name" placeholder="Nombres y apellidos" required>
             </div>
         </div>
-    </section>
+        <div class="form-group row mb-2">
+            <label for="email" class="col-sm-2 col-form-label">E-Mail</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control form-control-sm" id="email" name="email" placeholder="correo electrónico" required>
+            </div>
+        </div>
+        <div class="form-group row mb-2">
+            <label for="phone" class="col-sm-2 col-form-label">Teléfono / Móvil</label>
+            <div class="col-sm-10">
+                <input type="tel" class="form-control form-control-sm" id="phone" name="phone" placeholder="(+51) 9XX XXX XXX" required>
+            </div>
+        </div>
+        <div class="form-group row mb-2">
+            <label for="message" class="col-sm-2 col-form-label">Mensaje</label>
+            <div class="col-sm-10">
+                <textarea id="message" name="message" class="form-control form-control-sm" rows="4" placeholder="Escribe tu mensaje aquí..." required></textarea>
+            </div>
+        </div>
+        <div class="text-end mt-2">
+            <button type="submit" class="btn btn-primary btn-sm">Enviar mensaje</button>
+        </div>
+    </form>
+</div>
 
+<!-- Información de Contacto -->
+<div class="col-4">
+    <div class="card mb-3 p-3" style="border-radius: 10px; box-shadow: 0 2px 6px rgba(0,0,0,0.1);">
+        <div class="mb-3">
+            <h6 class="fw-bold">Correo</h6>
+            <p class="mb-1"><a href="mailto:ganasco@esg.com.pe">ganasco@esg.com.pe</a></p>
+            <p class="mb-0"><a href="mailto:ventas2@esg.com.pe">ventas2@esg.com.pe</a></p>
+        </div>
+        <div class="mb-3">
+            <h6 class="fw-bold">Teléfonos</h6>
+            <p class="mb-1">(+51) 1 713 1630</p>
+            <p class="mb-0">953 928 070</p>
+        </div>
+        <div>
+            <h6 class="fw-bold">Horario de Atención</h6>
+            <table class="table table-borderless mb-0">
+                <tr>
+                    <td>Lunes - Viernes<br>8:15 a.m. - 6:00 p.m.</td>
+                    <td>Sábado<br>8:15 a.m. - 12:30 p.m.</td>
+                </tr>
+            </table>
+        </div>
+    </section>
 </main>
 @endsection
 
@@ -99,7 +99,7 @@
 
 <script>
     function initMap() {
-        //var myLatLngC = {lat: -12.086019, lng: -77.025015};
+
         var myLatLng = {lat: -11.868842, lng: -77.126666};
 
         var map = new google.maps.Map(document.getElementById('mapCanvas'), {
